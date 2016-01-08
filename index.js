14507ec21e5558d80fb36ad23980f2c552784911
Matthew McCall <matt@mattmccall.com>
2016-01-08 11:15:48 -0800

    Basic Framework.

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ var dash_button = require('node-dash-button');
 var dash_config = require('./config.json');
 var dash = dash_button(dash_config.dash_list);
 
+function logEntry (callback) {
+
+
+	console.log('loggit')!
+
+}
+
+
+//Listen for initial dash config trigger.
 dash.on("detected", function (dash_id){
+
     if (dash_id === dash_config.dash_list[0]){
-        console.log("dash_config");
-    } else if (dash_id === dash_config.dash_list[1]){
-        console.log("found the other dash message!");
+        
+        logEntry(function (err) {
+        	if (err) {
+        		console.log(err);
+        	} else {
+        		console.log('entry saved');
+        	}
+        });
+
     }
+
 });
